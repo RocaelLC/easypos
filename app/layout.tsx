b2808dc61133756e739f77321dc06d202fc56fb4
RocaelLC <rocaellc42@gmail.com>
2025-12-30 22:57:45 -0600

    page for landing

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,10 +14,10 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
+      { url: "/icons/icon-192.jpg", sizes: "192x192", type: "image/jpeg" },
+      { url: "/icons/icon-512.jpg", sizes: "512x512", type: "image/jpeg" },
     ],
-    apple: "/icons/icon-192.png",
+    apple: "/icons/icon-192.jpg",
   },
 };
 
